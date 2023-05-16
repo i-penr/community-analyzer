7ebbb742ea86716e447be70b7d63f6df38b58ad5
i-penr <ismaelpenalvarodriguez@gmail.com>
2023-05-16 22:11:57 +0200

    Removed unused parameter

--- backend/bullmq/events/workerFailedEvent.ts
+++ backend/bullmq/events/workerFailedEvent.ts
@@ -2,7 +2,7 @@ import { Job } from "bullmq";
 
 module.exports = {
     name: 'failed',
-    async execute(job: Job, failedReason: string) {
+    async execute(job: Job) {
         console.error(`Job ${job.id} encountered an error`);
     }
 }

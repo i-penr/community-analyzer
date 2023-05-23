3054a7ae0423d88d2e49bc2191887198532cdd38
i-penr <ismaelpenalvarodriguez@gmail.com>
2023-05-23 16:54:09 +0200

    Added controller getPostsFromSub and cleaned other endpoints

--- backend/controllers/posts.ts
+++ backend/controllers/posts.ts
@@ -1,46 +1,26 @@
 import conn from '../db/db';
-import snoo from '../snoowrap/startConn';
 import { Db, ObjectId } from 'mongodb';
 import { Request, Response } from 'express';
 
 let db: Db;
 
 
-// POST
-export async function insertPosts(req: Request, res: Response) {
-    db = conn.getDb();
-    const sub: string = req.params.sub;
-    const limit = Number(req.params.limit);
-
-    const hottest = JSON.parse(JSON.stringify(await snoo.getSubreddit(sub).getHot({ limit: limit })));
-
-    db.collection('posts').insertMany(hottest).then((result) => {
-        res.status(201).send(result);
-    }).catch(err => {
-        console.log(err);
-        res.status(500).send({ err: 'Unable to insert post' });
-    });
-}
-
-export async function insertAllPosts(req: Request, res: Response) {
-    db = conn.getDb();
-    const sub = req.params.sub;
-
-    const hottest = JSON.parse(JSON.stringify(await fetchAllHotPosts(sub)));
-    
-    db.collection('posts').insertMany(hottest).then((result) => {
-        res.status(201).send(result);
-    }).catch(err => {
-        console.log(err);
-        res.status(500).send({ msg: 'Unable to insert post', err: err });
-    });
-}
-
 // GET
 export async function getPosts(req: Request, res: Response) {
-    db = conn.getDb();
+    conn.getDb().collection('posts').find({}).toArray().then((posts) => {
+        res.status(200).send(posts);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).send({ err: 'Unable to find documents' });
+    });
+}
 
-    db.collection('posts').find({}).toArray().then((posts) => {
+export async function getPostsFromSub(req: Request, res: Response) {
+    conn.getDb().collection('posts').find(
+        { subreddit: req.params.sub }
+    ).collation(
+        { locale: 'en', strength: 2 }
+    ).toArray().then((posts) => {
         res.status(200).send(posts);
     }).catch((err) => {
         console.log(err);
@@ -71,9 +51,4 @@ export async function deletePostById(req: Request, res: Response) {
             res.status(500).send({ err: 'Cannnot delete' });
         });
     }
-}
-
-
-export async function fetchAllHotPosts(sub: string) {
-    return (await snoo.getSubreddit(sub).getHot()).fetchAll({ amount: 0, append: true });
 }

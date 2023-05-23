01fe2ac101725831b0b2e67dc2b89dfbd34fb1a9
i-penr <ismaelpenalvarodriguez@gmail.com>
2023-05-23 16:54:55 +0200

    Removed testing endpoints

--- backend/routes/posts.ts
+++ backend/routes/posts.ts
@@ -1,12 +1,10 @@
 import express from 'express';
-import { getPosts, insertPosts, deleteAllPosts, deletePostById, insertAllPosts } from '../controllers/posts';
+import { getPosts, deleteAllPosts, deletePostById, getPostsFromSub } from '../controllers/posts';
 
 const router = express.Router();
 
 router.get('/all', getPosts);
-
-router.post('/insert/:sub&:limit', insertPosts);
-router.post('/insert/:sub', insertAllPosts);
+router.get('/:sub', getPostsFromSub);
 
 router.delete('/delete/all', deleteAllPosts);
 router.delete('/delete/:id', deletePostById);

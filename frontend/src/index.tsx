61be138a2fb20caae2e7aca70d3418eecd76ec2a
i-penr <ismaelpenalvarodriguez@gmail.com>
2023-05-31 21:54:52 +0200

    CSS imports

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import './css/index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement

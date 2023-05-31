bb320f007b6de154d04c424021486f92aa4eeb19
i-penr <ismaelpenalvarodriguez@gmail.com>
2023-05-31 21:52:20 +0200

    Added react-router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,29 @@
-import { CalHeatMap } from "./CalHeatMap";
-import { Input } from "./Input";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Home } from './Home';
+import { Navbar } from './Navbar';
+import { Sublist } from './Sublist';
+import { Subreddit } from './Subreddit';
 
 function App() {
-  
-
   return (
-    <div className="App">
-      <Input />
-      <CalHeatMap />
-    </div>
+    <Router>
+      <div className='App'>
+        <Navbar />
+        <div className='content'>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/sub/:sub'>
+            <Subreddit />
+          </Route>
+          <Route path='/subList'>
+            <Sublist />
+          </Route>
+        </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 

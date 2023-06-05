55932a8c15073fd094feea5a9b903a24f4ae2461
i-penr <ismaelpenalvarodriguez@gmail.com>
2023-06-05 20:56:54 +0200

    Changed user agent to comply with API terms

--- backend/snoowrap/startConn.ts
+++ backend/snoowrap/startConn.ts
@@ -3,7 +3,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const snoo = new Snoowrap({
-    userAgent: "Firefox",
+    userAgent: "Javascript:xdqifH3QLbkowQOrnJBeHw:v1.0Javascript:xdqifH3QLbkowQOrnJBeHw:v1.0",
     clientSecret: process.env.CLIENT_SECRET,
     clientId: process.env.CLIENT_ID,
     refreshToken: process.env.REFRESH_TOKEN

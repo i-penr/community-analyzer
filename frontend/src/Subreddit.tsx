760182cebc22677639262e638c8480a35ca9789c
i-penr <ismaelpenalvarodriguez@gmail.com>
2023-06-22 19:56:39 +0200

    Added CTA to add sub in Subreddit component

--- frontend/src/Subreddit.tsx
+++ frontend/src/Subreddit.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { useParams } from "react-router-dom";
 import { CalHeatMap } from "./CalHeatMap";
+import { useFetch } from "./customHooks/useFetch";
 import { Keywords } from "./Keywords";
 import { SubInfo } from "./SubInfo";
 import { SubscriberChart } from "./SubscriberChart";
@@ -10,25 +11,29 @@ interface ISubredditProps {
 
 export const Subreddit: FC = () => {
     const { sub } = useParams<ISubredditProps>();
+    const { data, error }: any = useFetch(`http://localhost:8080/subs/get/${sub}`);
 
     return (
         <div className="Subreddit">
-            <h1 className="title w-50 m-auto text-center py-4"><a href={`http://reddit.com/r/${sub}`}>r/{ sub }</a> Statistics</h1>
-            <div className="SubInfo Widget">
-                <SubInfo sub={sub} />
-            </div>
-            <div className="SubscriberChart Widget overflow-auto">
-                <h3 className="m-2 text-start">Subscriber Growth</h3>
-                <SubscriberChart sub={sub} />
-            </div>
-            <div className="CalHeatmap Widget overflow-auto">
-                <h3 className="m-2 text-start">Activity Calendar</h3>
-                <CalHeatMap sub={sub} />
-            </div>
-            <div className="Keywords Widget">
-                <h3 className="m-2 text-start">Top 50 Keywords</h3>
-                <Keywords sub={sub} />
-            </div>           
+            { error && <h4 className="text-center py-5">That subreddit does not exist in the database. Go to <a href="/addSub">add a subreddit</a> if you want to add it (if it exists).</h4>}
+            { data && !error && <div>
+                <h1 className="title w-50 m-auto text-center py-4"><a href={`http://reddit.com/r/${sub}`}>r/{sub}</a> Statistics</h1>
+                <div className="SubInfo Widget">
+                    <SubInfo sub={sub} />
+                </div>
+                <div className="SubscriberChart Widget overflow-auto">
+                    <h3 className="m-2 text-start">Subscriber Growth</h3>
+                    <SubscriberChart sub={sub} />
+                </div>
+                <div className="CalHeatmap Widget overflow-auto">
+                    <h3 className="m-2 text-start">Activity Calendar</h3>
+                    <CalHeatMap sub={sub} />
+                </div>
+                <div className="Keywords Widget">
+                    <h3 className="m-2 text-start">Top 50 Keywords</h3>
+                    <Keywords sub={sub} />
+                </div>
+            </div> }
         </div>
     );
 }
